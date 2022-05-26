42a1fd42748306cbefdefee6019ab32b1f182ad3
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-05-27 00:33:44 +0530

    added: Determine the Logging levels logging to console or file

--- src/main/java/com/example/addressbook/AddressBookApplication.java
+++ src/main/java/com/example/addressbook/AddressBookApplication.java
@@ -1,13 +1,17 @@
 package com.example.addressbook;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookApplication.class, args);
+        ApplicationContext context=SpringApplication.run(AddressBookApplication.class, args);
+        log.info("Address book started {} environment",context.getEnvironment().getProperty("environment"));
         System.out.println("Welcome to Address Book App..!");
     }
 
